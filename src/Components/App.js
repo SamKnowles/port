07845fa32ba21537ff345dc373cf1f6f25183951
Sam Knowles <samuelkcknowles@gmail.com>
2018-11-13 15:14:25 -0700

    added landing component

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Landing from './Landing'
+
+import '../Styles/App.css'
+import '../Styles/Landing.css'
+
+function App() {
+    return (
+        <div className='app-wrapper'>
+            <Landing />
+        </div>
+    )
+}
+
+export default App
---
+++ src/Components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Landing() {
+    return (
+        <div className='landing-wrapper'>
+            <div className='landing-center'></div>
+        </div>
+    )
+}
+
+export default Landing
