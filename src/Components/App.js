986fc4f600cbedc8d899f900dc99834d4d358873
Sam Knowles <samuelkcknowles@gmail.com>
2018-11-14 13:17:19 -0700

    added Mywork guts

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import Landing from './Landing'
+import MyWork from './MyWork'
+import Nav from './Nav'
 
+import '../Styles/Nav.css'
 import '../Styles/App.css'
 import '../Styles/Landing.css'
+import '../Styles/MyWork.css'
 
 function App() {
     return (
         <div className='app-wrapper'>
-            <Landing />
+            <div className='main-view-wrapper'>
+                <Landing />
+                <MyWork />
+            </div>
         </div>
     )
 }
