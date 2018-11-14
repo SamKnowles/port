a0644113af1d45acd1122ec1a206a7afa5ccbc2a
Sam Knowles <samuelkcknowles@gmail.com>
2018-11-14 13:23:19 -0700

    added formatting on mywork page

---
+++ src/Components/MyWork.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function MyWork() {
+    return (
+        <div className='my-work-wrapper'>
+            <div className='my-work-container'>
+                <div className='pic-maybe'></div>
+                <div className='work-list-container'>
+                    <div className='my-work'></div>
+                    <div className='my-work'></div>
+                    <div className='my-work'></div>
+                    <div className='my-work'></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MyWork
