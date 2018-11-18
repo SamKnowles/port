63b7f055f9ad265e0a91a3144141c405d027fbe9
Sam Knowles <samuelkcknowles@gmail.com>
2018-11-17 17:04:59 -0700

    added labels to mywork component

--- src/Components/MyWork.js
+++ src/Components/MyWork.js
@@ -6,26 +6,26 @@ function MyWork() {
             <div className='my-work-container'>
                 <div className='pic-maybe-container'>
                     <div className='pic-maybe'>
-                        <div className='pic'></div>
-                        <div className='my-info'></div>
+                        <div className='pic'>A picture of my face</div>
+                        <div className='my-info'>Here's information about me and shit</div>
                     </div>
                 </div>
                 <div className='work-list-container'>
                     <div className='my-work'>
-                        <div className='work'></div>
-                        <div className='work'></div>
+                        <div className='work'>Budget project screen shot</div>
+                        <div className='work'>Information about my Budget project</div>
                     </div>
                     <div className='my-work'>
-                        <div className='work'></div>
-                        <div className='work'></div>
+                        <div className='work'>Dad Jokes project screen shot</div>
+                        <div className='work'>Information about my Jokes app</div>
                     </div>
                     <div className='my-work'>
-                        <div className='work'></div>
-                        <div className='work'></div>
+                        <div className='work'>Calculator screen shot</div>
+                        <div className='work'>Information about my calculator app</div>
                     </div>
                     <div className='my-work'>
-                        <div className='work'></div>
-                        <div className='work'></div>
+                        <div className='work'>Color Changer screen shot</div>
+                        <div className='work'>Information about my color app</div>
                     </div>
                 </div>
             </div>
